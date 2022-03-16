7fc4a183ef7f4cba8b475028d8f4eebf5ce9c0ca
Anthony Vespoli <hi@anthonyvespoli.com>
2022-03-16 00:58:43 -0400

    animation wasn't working properly on gh-pages so i swapped it with the fade in slide animation

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,16 +1,20 @@
+import FadeInSection from "./FadeIn";
+
 const Hero = () => (
   <div className="hero-image">
     <div className="content-container">
       <div className="content">
-        <h1 className="fade-in">New Games &amp; Accessories</h1>
-        <span className="fade-in">Monthly Packages.</span>
-        <span className="fade-in">Excitement Delivered Daily.</span>
-        <p className="fade-in">
-          What's the best way to shop for the latest video games and
-          peripherals? How about never shopping at all? You'll get new stuff on
-          your doorstep - every month.
-        </p>
-        <button className="fade-in">Get Started</button>
+        <FadeInSection>
+          <h1>New Games &amp; Accessories</h1>
+          <span>Monthly Packages.</span>
+          <span>Excitement Delivered Daily.</span>
+          <p>
+            What's the best way to shop for the latest video games and
+            peripherals? How about never shopping at all? You'll get new stuff
+            on your doorstep - every month.
+          </p>
+          <button>Get Started</button>
+        </FadeInSection>
       </div>
     </div>
   </div>
